dce9a8ad3885a3381f06de27f7fad809457dea29
Keresztes Zsolt <77016531+kereszteszsolt@users.noreply.github.com>
2025-08-19 12:36:28 +0300

    chat page "base"

---
+++ src/app/features/chat/pages/chat-page/chat-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ollama-chat-chat-page',
+  imports: [],
+  templateUrl: './chat-page.component.html',
+  styleUrl: './chat-page.component.scss'
+})
+export class ChatPageComponent {
+
+}
