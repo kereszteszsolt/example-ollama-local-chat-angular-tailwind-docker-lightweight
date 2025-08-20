86c65c3fc4319238c18e9ff5ec0b407e89b0ef4a
Keresztes Zsolt <77016531+kereszteszsolt@users.noreply.github.com>
2025-08-20 12:28:16 +0300

    add prismjs for code syntax and mermaid for diagrams

--- src/app/features/chat/components/chat-page/chat-message/chat-message.component.ts
+++ src/app/features/chat/components/chat-page/chat-message/chat-message.component.ts
@@ -3,6 +3,32 @@ import { Message } from '../../../models/message.model';
 import { NgClass } from '@angular/common';
 import { MarkdownComponent } from 'ngx-markdown';
 
+import 'prismjs/components/prism-java.min.js';
+import 'prismjs/components/prism-python.min.js';
+import 'prismjs/components/prism-csharp.min.js';
+import 'prismjs/components/prism-pascal.min.js';
+import 'prismjs/components/prism-c.min.js';
+import 'prismjs/components/prism-cpp.min.js';
+import 'prismjs/components/prism-javascript.min.js';
+import 'prismjs/components/prism-typescript.min.js';
+import 'prismjs/components/prism-css.min.js';
+import 'prismjs/components/prism-scss.min.js';
+import 'prismjs/components/prism-json.min.js';
+import 'prismjs/components/prism-yaml.min.js';
+import 'prismjs/components/prism-latex.min.js';
+import 'prismjs/components/prism-matlab.min.js';
+import 'prismjs/components/prism-markup.min.js';
+import 'prismjs/components/prism-markup-templating.min.js';
+import 'prismjs/components/prism-dart.min.js';
+import 'prismjs/components/prism-docker.min.js';
+import 'prismjs/components/prism-xml-doc.min.js';
+import 'prismjs/components/prism-graphql.js';
+import 'prismjs/components/prism-php.min.js';
+import 'prismjs/components/prism-rust.min.js';
+import 'prismjs/components/prism-go.min.js';
+import 'prismjs/components/prism-sql.js';
+import 'prismjs/components/prism-plsql.min.js';
+
 @Component({
   selector: 'ollama-chat-chat-message',
   imports: [
