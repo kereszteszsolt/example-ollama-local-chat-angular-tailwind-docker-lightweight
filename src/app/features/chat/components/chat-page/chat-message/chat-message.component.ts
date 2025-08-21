37c0b9d803154d859675a6775d5087386553a347
Keresztes Zsolt <77016531+kereszteszsolt@users.noreply.github.com>
2025-08-21 15:59:10 +0300

    auto load languages, copy *.min.js from node_modules to public folder

--- src/app/features/chat/components/chat-page/chat-message/chat-message.component.ts
+++ src/app/features/chat/components/chat-page/chat-message/chat-message.component.ts
@@ -1,33 +1,7 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Message } from '../../../models/message.model';
 import { NgClass } from '@angular/common';
 import { MarkdownComponent } from 'ngx-markdown';
-
-import 'prismjs/components/prism-java.min.js';
-import 'prismjs/components/prism-python.min.js';
-import 'prismjs/components/prism-csharp.min.js';
-import 'prismjs/components/prism-pascal.min.js';
-import 'prismjs/components/prism-c.min.js';
-import 'prismjs/components/prism-cpp.min.js';
-import 'prismjs/components/prism-javascript.min.js';
-import 'prismjs/components/prism-typescript.min.js';
-import 'prismjs/components/prism-css.min.js';
-import 'prismjs/components/prism-scss.min.js';
-import 'prismjs/components/prism-json.min.js';
-import 'prismjs/components/prism-yaml.min.js';
-import 'prismjs/components/prism-latex.min.js';
-import 'prismjs/components/prism-matlab.min.js';
-import 'prismjs/components/prism-markup.min.js';
-import 'prismjs/components/prism-markup-templating.min.js';
-import 'prismjs/components/prism-dart.min.js';
-import 'prismjs/components/prism-docker.min.js';
-import 'prismjs/components/prism-xml-doc.min.js';
-import 'prismjs/components/prism-graphql.js';
-import 'prismjs/components/prism-php.min.js';
-import 'prismjs/components/prism-rust.min.js';
-import 'prismjs/components/prism-go.min.js';
-import 'prismjs/components/prism-sql.js';
-import 'prismjs/components/prism-plsql.min.js';
 
 @Component({
   selector: 'ollama-chat-chat-message',
@@ -38,6 +12,10 @@ import 'prismjs/components/prism-plsql.min.js';
   templateUrl: './chat-message.component.html',
   styleUrl: './chat-message.component.scss'
 })
-export class ChatMessageComponent {
+export class ChatMessageComponent implements OnInit {
   @Input({ required: true }) message!: Message;
+
+  ngOnInit(): void {
+    (window as any).Prism.plugins.autoloader.languages_path = 'prismjs-components/';
+  }
 }
