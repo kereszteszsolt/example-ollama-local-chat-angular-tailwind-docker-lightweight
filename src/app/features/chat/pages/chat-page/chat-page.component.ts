2587f96b24cd93b7e1b5bc2d4d3694a597b0bb01
Keresztes Zsolt <77016531+kereszteszsolt@users.noreply.github.com>
2025-08-22 13:01:52 +0300

    fix input scroll, fix user message, add auto scroll to the bottom

--- src/app/features/chat/pages/chat-page/chat-page.component.ts
+++ src/app/features/chat/pages/chat-page/chat-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { AfterViewChecked, Component, ElementRef, inject, ViewChild } from '@angular/core';
 import { ChatInputComponent } from '../../components/chat-page/chat-input/chat-input.component';
 import { OllamaService } from '../../services/ollama.service';
 import { ChatMessageComponent } from '../../components/chat-page/chat-message/chat-message.component';
@@ -12,11 +12,23 @@ import { ChatMessageComponent } from '../../components/chat-page/chat-message/ch
   templateUrl: './chat-page.component.html',
   styleUrl: './chat-page.component.scss'
 })
-export class ChatPageComponent {
+export class ChatPageComponent implements AfterViewChecked{
+  @ViewChild('scrollContainer') scrollContainer!: ElementRef<HTMLDivElement>;
   ollamaService = inject(OllamaService);
   messages = this.ollamaService.messageHistoryList;
   partialResponse = this.ollamaService.partialResponse;
   isLoading = this.ollamaService.isLoadingResponse;
+
+  ngAfterViewChecked() {
+    this.scrollToBottom();
+  }
+
+  private scrollToBottom() {
+    if (this.scrollContainer?.nativeElement) {
+      const el = this.scrollContainer.nativeElement;
+      el.scrollTop = el.scrollHeight;
+    }
+  }
 
   onSendMessage(message: string) {
     this.ollamaService.sendChatMessage(message);
